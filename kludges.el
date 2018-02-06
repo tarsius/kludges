0a1af1f6bdc693fa9de01eba14b90fd18b2e7bba
Jonas Bernoulli <jonas@bernoul.li>
2018-02-07 00:18:41 +0100

    Initial import

---
+++ kludges.el
@@ -0,0 +1,34 @@
+;;; kludges.el --- Assorted Elisp snippets        -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018  Jonas Bernoulli
+
+;; Author: Jonas Bernoulli <jonas@bernoul.li>
+;; Homepage: https://github.com/tarsius/kludges
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Assorted Elisp snippets intended for my own use.
+;; Things may appear and disappear at any time.
+
+;;; Code:
+
+(provide 'kludges)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+;;; kludges.el ends here
